1c4637f1baeb3f73407d222d85f7a70ba976ae4a
virajjayarathna <virajmjayarathna@gmail.com>
2025-03-19 11:11:52 +0530

    Update script to support HTML, CSS, YML, TSX, JSX

--- .github/scripts/add_copyright.sh
+++ .github/scripts/add_copyright.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-# Define the files to check (avoid .gitignore files)
-files=$(git ls-files | grep -E '\.(js|py|java|cpp|h|cs)$')
+# Define the file extensions to check
+files=$(git ls-files | grep -E '\.(js|jsx|ts|tsx|py|java|cpp|h|cs|html|css|yml|yaml)$')
 
 for file in $files; do
+  # Skip .gitignore files
+  if [[ "$file" == ".gitignore" ]]; then
+    continue
+  fi
+
+  # Determine the comment syntax based on file extension
+  case "$file" in
+    *.js|*.jsx|*.ts|*.tsx|*.java|*.cpp|*.h|*.cs)
+      comment="// Copyright (c) $(date +%Y) IT Solutions. All rights reserved.\n\n"
+      ;;
+    *.py|*.yml|*.yaml)
+      comment="# Copyright (c) $(date +%Y) IT Solutions. All rights reserved.\n\n"
+      ;;
+    *.html|*.css)
+      comment="<!-- Copyright (c) $(date +%Y) IT Solutions. All rights reserved. -->\n\n"
+      ;;
+    *)
+      continue
+      ;;
+  esac
+
+  # Check if the file already contains a copyright header
   if ! grep -q "Copyright" "$file"; then
-    current_year=$(date +%Y)
-    company_name="IT Solutions"
-    copyright_header="// Copyright (c) $current_year $company_name. All rights reserved.\n\n"
-    
-    # Add header to the file
-    echo -e "$copyright_header$(cat $file)" > "$file"
-    
-    # Stage the file
+    echo -e "$comment$(cat $file)" > "$file"
     git add "$file"
   fi
 done
